f556af4b778606445f0c0155a36154b9f43e5173
azza <asd@gmail.com>
2020-09-13 19:43:22 +0200

    first commit

---
+++ UI/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import{ToastrModule} from 'ngx-toastr'
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './user/login/login.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    RegistrationComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    ToastrModule.forRoot()
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Web API/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_API.Controllers
+{
+    public class LoginModel
+    {
+        public string UserName { get; internal set; }
+        public string Password { get; internal set; }
+    }
+}
